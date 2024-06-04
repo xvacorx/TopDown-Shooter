61f55c90fcd30381f30ae04e3bcb6fc10eeffac5
xvacorx <theproofthevictors@gmail.com>
2024-06-04 13:04:28 -0300

    tests

--- Assets/Scripts/Player/Weapons/BasicBullet.cs
+++ Assets/Scripts/Player/Weapons/BasicBullet.cs
@@ -2,14 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicBullet : MonoBehaviour
+public class BasicBullet : Bullet
 {
-    public float speed = 20f;
-    public float lifeTime = 5f;
-    private void Start()
-    {
-        Destroy(gameObject, lifeTime);
-    }
     void Update()
     {
         transform.Translate(Vector3.up * speed * Time.deltaTime);
--- Assets/Scripts/Player/Weapons/Bullet.cs
+++ Assets/Scripts/Player/Weapons/Bullet.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public abstract class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public float lifeTime;
+    private void Start()
     {
-        
+        Destroy(gameObject, lifeTime);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (!other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
